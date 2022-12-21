0f192ecdadb3f5886f46a7ca9785067d326e173d
Darya Oreshkina <darya.oreshkina@candid.org>
2022-12-21 17:39:39 -0500

    working sorting

--- algos/quicksort.py
+++ algos/quicksort.py
@@ -1,6 +1,5 @@
-COMP_CNT = 0
-def partiion(array, l_idx, r_idx):
-    pivot = choose_pivot(array, arr_len)
+
+def partiion(array, l_idx, r_idx, pivot):
     i = l_idx + 1
     j = l_idx + 1
     while j <= r_idx:
@@ -9,13 +8,12 @@ def partiion(array, l_idx, r_idx):
             array[i] = array[j]
             array[j] = swap
             i += 1
+        j += 1
     swap = array[i-1]
     array[i-1] = array[l_idx]
-    array[l_idx] = swap        
+    array[l_idx] = swap
 
-
-
-
+    return array
 
 
 def choose_pivot(array, arr_len):
@@ -23,24 +21,27 @@ def choose_pivot(array, arr_len):
 
 
 def quick_sort(array, arr_len):
-    global COMP_CNT += arr_len - 1
+    number_of_comparisons = 0
     if arr_len == 1:
-        return array, number_of_comparisons
+        return array
     else:
-        
-        left = array[:pivot_idx]
-        right = array[pivot_idx: array]
-        quick_sort(left, len(left))
-        quick_sort(right, len(right))
+        pivot = choose_pivot(array, arr_len)
+        partitioned_array = partiion(array, 0, len(array) - 1, pivot)
+        left = partitioned_array[: partitioned_array.index(pivot) + 1]
+        right = partitioned_array[partitioned_array.index(pivot) + 1 :]
+        sorted_l = quick_sort(left, len(left))
+        sorted_r = quick_sort(right, len(right))
+        array = sorted_l + sorted_r
 
-        return array, number_of_comparisons
+        return array
 
 
-with open ('quicksort.txt', 'r') as f:
+with open ('quicksort1.txt', 'r') as f:
     arr = []
     arr_len = 0
     d = f.read().split()
     for line in d:
         arr.append(int(line))
         arr_len+=1
-    print(quick_sort(arr, arr_len)[1])
+    print(quick_sort(arr, arr_len))
+    
