ee831048a6fd82b0975c957dbe9f48cd224b4012
Darya Oreshkina <darya.oreshkina@candid.org>
2023-01-02 17:58:00 -0500

    fixed karger

--- algos/karger_graph_mincut.py
+++ algos/karger_graph_mincut.py
@@ -1,19 +1,22 @@
 import random
+from copy import deepcopy
 
 def find_min_cut(edges, num_of_vertices):
+    arcs = deepcopy(edges)
     while num_of_vertices > 2:
-        rand_edge = random.choice(edges)
-        edges.pop(edges.index(rand_edge))
-        num_of_vertices -= 1
+        rand_edge = random.choice(arcs)
+        arcs.pop(edges.index(rand_edge)) # deleting first directed part of the contracted edge
         u = rand_edge[0]
         v = rand_edge[1]
+        arcs.pop(arcs.index([v,u])) # deleting second directed part of the contracted edge
+        num_of_vertices -= 1
         contracted_edges = []
-        for edge in edges:
-            edge = [v if x == u else x for x in edge]
-            if not [v,v] == edge:
+        for e in arcs:
+            edge = [v if x == u else x for x in e] # replacing contracted edge with the one that it is merging to
+            if not [v,v] == edge: # we don't need self loops
                 contracted_edges.append(edge)
         return find_min_cut(contracted_edges, num_of_vertices)
-    return len(edges)
+    return len(arcs)/2 # dividing by two because we count both directions
 
 with open ('kargerMinCut.txt', 'r') as f:
     edges = []
@@ -25,8 +28,10 @@ with open ('kargerMinCut.txt', 'r') as f:
         for vertex in vertices:
             if vertex == vertex_main:
                 continue
-            edges.append((vertex_main, vertex))
-    print(find_min_cut(edges, vertices_num))
+            edges.append([vertex_main, vertex])
 
-    # 50 42 40
-    # 
+    min_cuts = []
+    for i in range(0,200):
+        min_cut = (find_min_cut(edges, vertices_num))
+        min_cuts.append(min_cut)
+    print(min(min_cuts))
